1fe024e6629dad007f19cce4929e2095705320ae
Amit <amitnaipaul@gmail.com>
2021-04-28 11:36:29 -0400

    seed info

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,7 +20,6 @@ Demons = Game.create!(name: "Demon's Souls", image: "https://images-na.ssl-image
 Ghosts = Game.create!(name: "Ghosts of Tsushima", image: "https://images-na.ssl-images-amazon.com/images/I/81Bzm37TIaL._SL1500_.jpg")
 Outlast = Game.create!(name: "Outlast", image: "https://images-na.ssl-images-amazon.com/images/I/51UyDUpC9kL._AC_.jpg")
 Bloodborne = Game.create!(name: "Bloodborne", image: "https://images-na.ssl-images-amazon.com/images/I/81Z-g%2BJU%2BOL._SL1500_.jpg")
-
 RedDead = Game.create!(name: "Read Dead Redemption 2", image: "https://images-na.ssl-images-amazon.com/images/I/81zYK3DIv9L._SL1500_.jpg")
 Spider = Game.create!(name: "Marvel's Spider-Man", image: "https://images-na.ssl-images-amazon.com/images/I/814tECNqHBL._SL1500_.jpg")
 God = Game.create!(name: "God of War", image: "https://images-na.ssl-images-amazon.com/images/I/711lrrvzt%2BL._SL1496_.jpg")
@@ -30,12 +29,13 @@ Finalfantasy = Game.create!(name: "Final Fantasy 15", image: "https://images-na.
 Metalgear = Game.create!(name: "Metal Gear Solid 5", image: "https://images-na.ssl-images-amazon.com/images/I/911Y1BkwReL._SL1500_.jpg")
 
 
-review1 = Review.create!(user_id: 1, game_id: 1, rating: 7, comments:"Long and sometimes confusing.")
-review2 = Review.create!(user_id: 1, game_id: 2, rating: 8, comments:"Compelling but sometimes confusing storyt")
-review3 = Review.create!(user_id: 1, game_id: 3, rating: 9, comments:"Remaster of a Yoko Taro classic")
-review4 = Review.create!(user_id: 1, game_id: 4, rating: 10, comments:"Remaster of the first From souls-like game.")
-review5 = Review.create!(user_id: 1, game_id: 5, rating: 8, comments:"Great open world samurai simulator")
-review6 = Review.create!(user_id: 1, game_id: 6, rating: 7, comments:"Scary and gory.")
+review1 = Review.create!(user_id: 1, game_id: 1, rating: 7, comments: "Long and sometimes confusing.")
+review2 = Review.create!(user_id: 1, game_id: 2, rating: 8, comments: "Compelling but sometimes confusing storyt")
+review3 = Review.create!(user_id: 1, game_id: 3, rating: 9, comments: "Remaster of a Yoko Taro classic")
+review4 = Review.create!(user_id: 1, game_id: 4, rating: 10, comments: "Remaster of the first From souls-like game.")
+review5 = Review.create!(user_id: 1, game_id: 5, rating: 8, comments: "Great open world samurai simulator")
+review6 = Review.create!(user_id: 1, game_id: 6, rating: 7, comments: "Scary and gory.")
+review7 = Review.create!(user_id: 1, game_id: 7, rating: 10, comments: "Best game ever!")
 
 
 
